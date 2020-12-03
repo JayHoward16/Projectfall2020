4e1c84d91837435917a2996e39fe3de6a56fd9de
Josiah Howard <46724764+JayHoward16@users.noreply.github.com>
2020-12-03 00:54:07 -0500

    Add files via upload

--- loginjm.php
+++ loginjm.php
@@ -12,6 +12,25 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>J & M Rentals</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+  <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+    integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc"
+    crossorigin="anonymous"
+  />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="main.js"></script>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <style>
   body {
   background-image: url('images/regbg.jpg');
@@ -24,7 +43,30 @@ session_start();
   </style>
 </head>
 <body>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+<a class="navbar-brand" href="home.php">J & M Rentals</a>
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div id="navb" class="navbar-collapse collapse hide">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="home.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://github.com/JayHoward16/Projectfall2020/blob/main/README.md">About us</a>
+    </li>
 
+  </ul>
+
+      <ul class="nav navbar-nav ml-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" href="registrationform.php"><span class="fas fa-user"></span> Sign Up</a>
+        </li>
+  </ul>
+</div>
+</nav>
 
      <form action='' class='form' method='POST'>
 
@@ -90,9 +132,9 @@ session_start();
 
       if($count == 1) {
 
-         $_SESSION['login_user'] =  $Username;
+         $_SESSION['login_user'] =  $UserNAME;
 
-         header("location: welcomelab5.php");
+         header("location: home.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
